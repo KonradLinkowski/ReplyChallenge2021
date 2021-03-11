a4fed955e9ac3bacdc96d45c028c82ad4f3cab14
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-03-11 16:44:48 +0100

    Add test cases

--- src/loader.js
+++ src/loader.js
@@ -4,11 +4,11 @@ const inputDirectory = 'input'
 
 async function load(dataIdentifier) {
   const files = await readdir(inputDirectory)
-  const file = files.find(file => file.startsWith(dataIdentifier))
+  const file = files.find(file => file.includes(`_${dataIdentifier}_`))
   if (!file) {
-    throw new Error(`No input file starts with ${dataIdentifier}`)
+    throw new Error(`No input file for ${dataIdentifier}`)
   }
-  return await readFile(join(inputDirectory, file))
+  return await readFile(join(inputDirectory, file), 'utf-8')
 }
 
 module.exports = {
